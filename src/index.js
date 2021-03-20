e33b6cf428c9fb932be4a7d29ca1bfc4ae8e50b7
Christopher Geary <hello@crgeary.com>
2021-03-20 12:36:57 +0000

    Add utilities for retina/resolution support

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import Phaser from 'phaser';
 
-import './css/main.css';
+import { ZOOM, scaleUnit } from './utils/dpr';
 import { BootScene, PreloadScene, MenuScene, MainScene } from './scenes';
+
+import './css/main.css';
 
 new Phaser.Game({
     type: Phaser.AUTO,
     scene: [BootScene, PreloadScene, MenuScene, MainScene],
+    width: scaleUnit(500),
+    height: scaleUnit(500),
+    zoom: ZOOM,
 });
